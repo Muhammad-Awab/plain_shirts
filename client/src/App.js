eba8cd3f7c7ed7b0117796bf2ea494b418f67460
AbdulAhad210904 <210904@students.au.edu.pk>
2024-04-25 15:37:43 +0500

    Landing Page Done!

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import Main from "./components/Main";
 import Signup from "./components/Singup";
 import Login from "./components/Login";
+import LandingPage from "./components/Landing_page";
 
 function App() {
 	const user = localStorage.getItem("token");
@@ -11,7 +12,7 @@ function App() {
 			{user && <Route path="/" exact element={<Main />} />}
 			<Route path="/signup" exact element={<Signup />} />
 			<Route path="/login" exact element={<Login />} />
-			<Route path="/" element={<Navigate replace to="/login" />} />
+			<Route path="/" exact element={<LandingPage/>} />
 		</Routes>
 	);
 }
