405fd5b58ec1ac7945fe9f0d651e3909279960bc
Muhammad-Awab <muhammadawab82002@gmail.com>
2024-05-12 11:46:06 +0500

    Add Cart page, Add Checkoout Page, Add products

--- Backened/controller/userLogout.js
+++
@@ -1,8 +0,0 @@
-async function userLogout(req, res){
-    try {
-        res.clearCookie("token").json({ message: "User Logged Out Successfully", success: true, error: false });
-    } catch (error) {
-        res.json({ message: err, success: false, error: true });
-    }
-}
-module.exports=userLogout;
