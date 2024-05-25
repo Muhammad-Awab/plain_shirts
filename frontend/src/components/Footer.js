4008d069d2c3641c0a4a55a791f3779a5295ef5b
Muhammad Taha Shayan <127018267+Tahashayan@users.noreply.github.com>
2024-05-25 23:47:16 +0500

    Add logo to footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,10 +1,14 @@
 import React from 'react'
-
+import Logo from './Logo'
+import { Link } from 'react-router-dom'
 const Footer = () => {
   return (
     <footer className='bg-slate-200'>
-      <div className='container mx-auto p-4'>
-       <p className='text-center font-bold' title="Youtube Channel">Dynamic Coding with Amit</p>
+      <div className='container mx-auto p-4 flex flex-col items-center'>
+      <Link to={"/"}>
+                <Logo w={110} h={100} style={{ borderRadius: '50%', overflow: 'hidden' }} />
+            </Link>
+        <p className='text-center font-bold'>Iconic store</p>
       </div>
     </footer>
   )
