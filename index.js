9cbd36ca9a21c98494ddaf5a79fb31e5773e82c7
Muhammad-Awab <muhammadawab82002@gmail.com>
2024-05-27 21:52:19 +0500

    Fix Cors Error

--- index.js
+++ index.js
@@ -5,23 +5,32 @@ require('dotenv').config()
 const connectDB = require('./config/db')
 const router = require('./routes')
 
-
 const app = express()
 
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001','https://plain-shirts-jwnr.vercel.app']
+
 app.use(
-    cors({credentials: true, origin: true})
+    cors({
+        credentials: true,
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true)
+            } else {
+                callback(new Error('Not allowed by CORS'))
+            }
+        }
+    })
 )
 app.use(express.json())
 app.use(cookieParser())
 
-app.use("/api",router)
+app.use("/api", router)
 
-const PORT = 8080 || process.env.PORT
+const PORT = process.env.PORT || 8080
 
-
-connectDB().then(()=>{
-    app.listen(PORT,()=>{
-        console.log("connnect to DB")
-        console.log("Server is running "+PORT)
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Connected to DB")
+        console.log("Server is running on port " + PORT)
     })
 })
