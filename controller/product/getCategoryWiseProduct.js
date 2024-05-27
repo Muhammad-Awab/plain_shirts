7e755b3e50858da3493e98dbdfa8355a3e4a76c7
Muhammad-Awab <muhammadawab82002@gmail.com>
2024-05-27 22:29:02 +0500

    Fix deployment backened error

--- controller/product/getCategoryWiseProduct.js
+++ controller/product/getCategoryWiseProduct.js
@@ -1,8 +1,7 @@
 const productModel = require("../../models/productModel")
 
 const getCategoryWiseProduct = async(req,res)=>{
-    res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    
     try{
         const { category } = req?.body || req?.query
         const product = await productModel.find({ category })
--- controller/product/getProduct.js
+++ controller/product/getProduct.js
@@ -1,8 +1,7 @@
 const productModel = require("../../models/productModel")
 
 const getProductController = async(req,res)=>{
-    res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    
     try{
         const allProduct = await productModel.find().sort({ createdAt : -1 })
 
